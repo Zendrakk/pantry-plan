127c9d3e94f6e1e786c7bdf2a68753e7af7bc92e
Zendrakk <cc.nieman@icloud.com>
2026-07-10 22:59:18 -0500

    Add Identity schema, EF Core + Neon Postgres integration, initial migration

---
+++ server/Domain/User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace PantryPlan.Api.Domain
+{
+    // IdentityUser already gives: Id (string, GUID by default), UserName, Email, PasswordHash, PhoneNumber, EmailConfirmed,
+    // security stamp, lockout fields, and more.
+    public class User : IdentityUser
+    {
+    }
+}
---
+++ server/Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PantryPlan.Api.Domain;
+
+namespace PantryPlan.Api.Infrastructure.Persistence
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext<User>(options)
+    {
+    }
+}
